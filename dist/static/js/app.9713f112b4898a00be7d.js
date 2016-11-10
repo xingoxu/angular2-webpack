df923f1ab4bdb6d7bebdfcefd8edf0e4449ecc2c
xingo <xingoxu@gmail.com>
2016-11-10 21:26:54 +0800

    reconfig webpack

--- dist/static/js/app.9713f112b4898a00be7d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,0,2],[function(n,o){!function(n){document.addEventListener("DOMContentLoaded",function(){ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(n.AppModule)})}(window.app||(window.app={}))}]);
-//# sourceMappingURL=app.9713f112b4898a00be7d.js.map
